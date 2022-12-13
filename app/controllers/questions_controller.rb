0a77f7e124c98b8ca34af7ebae79fbbdd5176b5f
Shou1204 <flyingsheep0000@gmail.com>
2022-12-13 11:30:52 +0900

    index,newの実装

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @questions = Question.new
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Rails.application.routes.draw do
+  root to: "questions#index"
+  resources :questions, only: [:index,:new]
+  # get "questions" ,to: "questions#index"
+  # post "questions/new",to: "questions#new"
 end
